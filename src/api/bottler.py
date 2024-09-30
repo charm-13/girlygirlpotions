762149872fabacb468a6027ffcac00b307f5b294
Charlotte Maples <cgmaples13@gmail.com>
2024-09-29 17:14:41 -0700

    updated bottler

--- src/api/bottler.py
+++ src/api/bottler.py
@@ -17,12 +17,18 @@ class PotionInventory(BaseModel):
 
 @router.post("/deliver/{order_id}")
 def post_deliver_bottles(potions_delivered: list[PotionInventory], order_id: int):
-    # with db.engine.begin() as connection:
-    #     result = connection.execute(sqlalchemy.text(sql_to_execute))
     """ """
+    potions = 0
+    for potion in potions_delivered:
+        potions += potion.quantity
+    
+    with db.engine.begin() as connection:
+        connection.execute(sqlalchemy.text("UPDATE global_inventory SET num_green_potions = num_green_potions + :potions"),
+                           {"potions": potions})
+        
     print(f"potions delievered: {potions_delivered} order_id: {order_id}")
 
-    return "OK"
+    return {"status": "success", "potions": potions}
 
 @router.post("/plan")
 def get_bottle_plan():
@@ -33,16 +39,23 @@ def get_bottle_plan():
     # green potion to add.
     # Expressed in integers from 1 to 100 that must sum up to 100.
 
-    # Initial logic: bottle all barrels into red potions.
-    # with db.engine.begin() as connection:
-    #     result = connection.execute(sqlalchemy.text(sql_to_execute))
+    # Initial logic: bottle all barrels into green potions.
+    
+    with db.engine.begin() as connection:
+        result = connection.execute(sqlalchemy.text("SELECT num_green_ml FROM global_inventory")).mappings()
+        inventory = result.fetchone()
+        num_green_ml = inventory["num_green_ml"]
+        
+    green_potions_to_bottle = num_green_ml // 100
+        
+    plan = [
+        {
+            "potion_type": [0, 100, 0, 0],
+            "quantity": green_potions_to_bottle,
+        }
+    ]
 
-    return [
-            {
-                "potion_type": [100, 0, 0, 0],
-                "quantity": 5,
-            }
-        ]
+    return plan
 
 if __name__ == "__main__":
     print(get_bottle_plan())
