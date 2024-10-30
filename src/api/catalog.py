57a8f16de4a0c88f98b94de4a0ea85ee13a145dc
Charlotte Maples <cgmaples13@gmail.com>
2024-10-29 21:34:24 -0700

    update catalog to handle more than 6 potions

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -10,29 +10,33 @@ def get_catalog():
     """
     Provides all available potions.
     """
+    #TODO: implement good logic for what to offer when
     with db.engine.begin() as connection:
         inv_result = connection.execute(
             sqlalchemy.text("""SELECT potion_inventory.sku, name, quantity, price, red_amt, 
                                     green_amt, blue_amt, dark_amt 
                             FROM potion_inventory
+                            WHERE quantity > 0
+                            ORDER BY quantity desc
                             LIMIT 6""")
         ).mappings()
         
     catalog = []
     
+    
     for potion in inv_result:
+        print(f"inv: {potion}")
         potion_type = [potion["red_amt"], potion["green_amt"], potion["blue_amt"], potion["dark_amt"]]
+    
+        catalog.append(
+            {
+                "sku": potion["sku"],
+                "name": potion["name"],
+                "quantity": potion["quantity"],
+                "price": potion["price"],
+                "potion_type": potion_type
+            }
+        )
         
-        if potion["quantity"] > 0:
-            catalog.append(
-                {
-                    "sku": potion["sku"],
-                    "name": potion["name"],
-                    "quantity": potion["quantity"],
-                    "price": potion["price"],
-                    "potion_type": potion_type
-                }
-            )
-            
     print(f"Catalog: {catalog}")        
     return catalog
